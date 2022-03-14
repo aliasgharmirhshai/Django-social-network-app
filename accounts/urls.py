327845af1dc3008ff91ebd3c5f1d9e7fd452eae9
aliasgharmirhshai <lmyrshahy3@gmail.com>
2022-03-14 09:40:45 -0400

    Password reset

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,12 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='user_login'),
     path('logout/', auth_views.LogoutView.as_view(), name='user_logout'),
     path('', views.dashboard, name='user_dashboard'),
+    # Reset Password
     path('password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
     path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    # Forgotten Password
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
