e3ef6b79d854b5085502877e66a7710e40422348
aliasgharmirhshai <lmyrshahy3@gmail.com>
2022-03-22 03:19:14 -0400

    Add Slugify

--- images/models.py
+++ images/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.utils.text import slugify
 # Create your models here.
 
 class Image(models.Model):
@@ -13,3 +14,8 @@ class Image(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
